8821d5c3c2e7a9514e55f5ea81b9785e7eaadd78
daszybak <karlo1diidd@gmail.com>
2022-05-31 22:00:09 +0200

    fixed useLayoutEffect error ssr problem

--- containers/footbar/footbar.jsx
+++ containers/footbar/footbar.jsx
@@ -1,27 +1,34 @@
 import styles from './footbar.module.scss';
 import Form from '../form/form';
 import CopyrightCard from '../../components/copyrightCard/copyrightCard';
-import {useRef} from 'react';
+import {useRef, useState, useEffect} from 'react';
 import useIntersection from '../../hooks/useIntersection';
 import Link from 'next/link';
 import {IoMailOutline} from 'react-icons/io5';
 
 const Footbar = () => {
   const footbarContainer = useRef();
-  const isFootbarConatinerVisible = useIntersection(footbarContainer);
+  const [showButton, setShowButton] = useState(false);
+  const isFootbarContainerVisible = useIntersection(footbarContainer);
+
+  useEffect(() => {
+    setShowButton(true);
+  }, []);
 
   return (
     <footer className={styles.footer} ref={footbarContainer}>
       <div id="footer" className={styles.anchor}></div>
       <Form />
-      {!isFootbarConatinerVisible && (
-        <Link href="/#footer">
-          <a className={styles['menu-btn']} style={{height: 'auto'}}>
-            <IoMailOutline />
-            &nbsp;Naručite se
-          </a>
-        </Link>
-      )}
+      {showButton
+        ? !isFootbarContainerVisible && (
+            <Link href="/#footer">
+              <a className={styles['menu-btn']} style={{height: 'auto'}}>
+                <IoMailOutline />
+                &nbsp;Naručite se
+              </a>
+            </Link>
+          )
+        : null}
       <CopyrightCard />
     </footer>
   );
